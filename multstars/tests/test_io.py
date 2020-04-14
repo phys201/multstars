8b1385ea3ec9d77be901e59b7ad825ef5c94f21c
vditomasso <victoria.ditomasso@macaulay.cuny.edu>
2020-04-14 14:02:13 -0400

    add test_io.py

---
+++ multstars/tests/test_io.py
@@ -0,0 +1,10 @@
+# edited from phys201 example
+
+from unittest import TestCase
+from multstars.data_io import get_example_data_file_path, load_data
+import pandas as  pd
+
+class TestIo(TestCase):
+    def test_data_io(self):
+        data = load_data(get_example_data_file_path('doubles_test.txt'))
+        assert data.LSPM_ID[0] == 'J0324+3804'

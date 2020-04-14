4143d3023154717f7e82f9887c66f2b1776da16f
jgillis16 <47949848+jgillis16@users.noreply.github.com>
2020-04-14 18:50:19 -0400

    replacing old setup.py file
    fixed semantics

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='multstars',
       version='1.0',
-      description='This is a package built to infer and identify multiplicity trends in the Robo-AO M-dwarf multiplicity survey through identifying distributions towards equal-mass binaries and how mass ratios vary with separation.',
+      description='A package built to infer and identify trends in the projected semi-major axes of binaries in the Robo-AO M-dwarf multiplicity survey.',
       url='http://github.com/phys201/multstars',
       author='Jasmine, Claire, and Victoria',
       author_email='jasmine.gill@cfa.harvard.edu',
@@ -11,5 +11,5 @@ setup(name='multstars',
 		'multstars.model'],
       install_requires=['numpy','matplotlib','astropy','pandas','seaborn'],
       test_suite='nose.collector',
-      tests_require=['nose'])
+      tests_require=['nosetests'])
 

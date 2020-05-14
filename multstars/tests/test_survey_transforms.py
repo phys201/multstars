50130a3144b85a056c1c522b32ddf78e0cafd083
vditomasso <victoria.ditomasso@macaulay.cuny.edu>
2020-05-13 22:06:18 -0400

    add test_imr_to_cr

--- multstars/tests/test_survey_transforms.py
+++ multstars/tests/test_survey_transforms.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from multstars.survey_transformations import imr_to_icr
+from multstars.survey_transformations import *
 
 class test_imr_to_icr(TestCase):
 
@@ -8,3 +8,11 @@ class test_imr_to_icr(TestCase):
 
     def test_calc(self):
         self.assertAlmostEqual(imr_to_icr(2), 0.512, 3)
+
+class test_imr_to_cr(TestCase):
+
+    def test_output_type(self):
+        self.assertTrue(isinstance(imr_to_cr(2),float))
+
+    def test_calc(self):
+        self.assertAlmostEqual(imr_to_cr(2), 1.952, 3)

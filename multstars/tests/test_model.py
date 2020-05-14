d17205527071f9cdfb1940dd0522ca7ba5c0e8e9
vditomasso <victoria.ditomasso@macaulay.cuny.edu>
2020-05-13 22:07:30 -0400

    test that model runs

--- multstars/tests/test_model.py
+++ multstars/tests/test_model.py
@@ -1,28 +1,14 @@
 from unittest import TestCase
-from multstars.model import log_likelihood, pymc3_fit
+from multstars.model import *
+from multstars.data_io import *
 import pandas
 
-fake_data = pandas.DataFrame({'SEP_PHYSICAL':[1,1,5,5,5,5,5,5,10,10,10,10,10,10,10,10,10,10,15,15,15,15,15,15,19,19],'E_SEP_PHYSICAL':[0]*26})
+data_path = get_example_data_file_path('hm_data_test.txt')
+data = load_data(data_path)
 
-log_L = log_likelihood(fake_data,10,5)
-fit_df = pymc3_fit(fake_data, nsteps=400, center_max=20)
-q = fit_df.quantile([0.16,0.50,0.84], axis=0)
+traces, samples = pymc3_hrchl_fit(data.sample(10),tune=10,nsteps=10)
 
 class model(TestCase):
 
-    def test_log_likelihood_type(self):
-        self.assertTrue(isinstance(log_L, float))
-
-    def test_log_likelihood_calc(self):
-        self.assertTrue(-42 <= log_L <= -41)
-
-    def test_pymc3_fit_type(self):
-        self.assertTrue(isinstance(fit_df, pandas.core.frame.DataFrame))
-
-    def test_pymc3_fit_center_value(self):
-        c_lower, c, c_upper = q['center']
-        self.assertTrue(9 <= c <= 11)
-
-    def test_pymc3_fit_width_value(self):
-        w_lower, w, w_upper = q['width']
-        self.assertTrue(0 <= w <= 10)
+    def test_output_type(self):
+        self.assertTrue(isinstance(samples,pandas.core.frame.DataFrame))

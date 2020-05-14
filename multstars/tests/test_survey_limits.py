33381660517634bf4ec2e24c57fc66f6635bab9c
vditomasso <victoria.ditomasso@macaulay.cuny.edu>
2020-05-13 22:05:22 -0400

    tests for all 4 funcs

--- multstars/tests/test_survey_limits.py
+++ multstars/tests/test_survey_limits.py
@@ -1,13 +1,5 @@
 from unittest import TestCase
-from multstars.survey_limits import icr_max, sep_min
-
-class test_icr_max(TestCase):
-
-    def test_output_type(self):
-        self.assertTrue(isinstance(icr_max(2),float))
-
-    def test_calc(self):
-        self.assertAlmostEqual(icr_max(2), 0.198, 3)
+from multstars.survey_limits import *
 
 class test_sep_min(TestCase):
 
@@ -15,4 +7,22 @@ class test_sep_min(TestCase):
         self.assertTrue(isinstance(sep_min(2),float))
 
     def test_calc(self):
-        self.assertAlmostEqual(sep_min(2), 0.16, 2)
+        self.assertAlmostEqual(sep_min(2), 0.37, 2)
+
+class test_sep_max(TestCase):
+
+    def test_output_type(self):
+        self.assertTrue(isinstance(sep_max(2), float))
+
+class test_cr_max(TestCase):
+
+    def test_output_type(self):
+        self.assertTrue(isinstance(cr_max(2),float))
+
+    def test_calc(self):
+        self.assertAlmostEqual(cr_max(2), 5.048, 3)
+
+class test_cr_min(TestCase):
+
+    def test_output_type(self):
+        self.assertTrue(isinstance(cr_min(2), float))
